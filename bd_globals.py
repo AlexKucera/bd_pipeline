c51f0079472ed28cbb32d366fbe1b68695c15d95
Alexander Kucera <a.kucera@babylondreams.de>
2017-03-09 14:44:13 +0100

    First working version
    Signed-off-by: Alexander Kucera <a.kucera@babylondreams.de>

--- bd_globals.py
+++ bd_globals.py
@@ -13,35 +13,130 @@ Release Notes:
 V0.1 Initial Release
 
 """
-
-import traceback
+import os
 import json
+
+import re
+
+from vars import *
 
 
 # FUNCTIONS -----------------------------------------------
+class QueryDict(dict):
+    def __getitem__(self, key_string):
+        current = self
+        try:
+            for key in key_string.split('/'):
+                current = dict.__getitem__(current, key)
+            return current
+        except (TypeError, KeyError):
+            return None
+
+
+def walk_up(bottom):
+    """
+    mimic os.walk, but walk 'up'
+    instead of down the directory tree
+
+    os.walk is an awesome generator.
+    However, I needed the same functionality, only I wanted to walk 'up' the directory tree.
+    This allows searching for files in directories directly above a given directory.
+
+    via: https://gist.github.com/zdavkeos/1098474
+    """
+
+    bottom = os.path.realpath(bottom)
+    if os.path.isfile(bottom):
+        bottom = os.path.dirname(bottom)
+
+    # get files in current dir
+    try:
+        names = os.listdir(bottom)
+    except Exception as e:
+        print(e)
+        return
+
+    dirs, nondirs = [], []
+    for name in names:
+        if os.path.isdir(os.path.join(bottom, name)):
+            dirs.append(name)
+        else:
+            nondirs.append(name)
+
+    yield bottom, dirs, nondirs
+
+    new_path = os.path.realpath(os.path.join(bottom, '..'))
+
+    # see if we are at the top
+    if new_path == bottom:
+        return
+
+    for x in walk_up(new_path):
+        yield x
+
+
+def find_project(filepath):
+
+    """
+    Tries to find the project folder for any given file based on the global pipeline config.
+    :param filepath:
+    :return:
+        None if no project was found
+        project (dict) containing project directory, project name and client name if a project was found.
+    """
+
+    regex_string = "({0})([^/]+)/([^/]+)".format(bdconfig()['projects dir'])
+    regex = re.compile(regex_string)
+    match = re.match(regex, filepath)
+
+    if match is not None:
+        client = match.group(2)
+        project = match.group(3)
+        project_dir = match.group()
+
+        return {'project_dir': project_dir, 'client': client, 'project': project}
+
+    else:
+
+        return None
+
+
+def bdconfig():
+    """
+    Returns a dictionary with found global pipeline data based on the global project definitions.
+
+    :return: babvars (dict)
+    """
+
+    with open(CONFIG_PATH) as json_data:
+        bab_vars = QueryDict(json.load(json_data))
+
+    bab_vars['projects dir'] = "{drive}{sep}{projects}{sep}".format(drive=bab_vars['project drive'],
+                                                                   projects=bab_vars['projects location'],
+                                                                   sep=os.sep)
+
+    return bab_vars
+
+
+def projectconfig(filepath):
+    """
+    Returns a dictionary with found project data based on the global project definitions.
+    If a valid project path is found it tries to reads it's project config. If none exists the global config is used.
+    :param filepath:
+    :return: projectvars (dict)
+    """
+    project = find_project(filepath)
+    bdconfigs = bdconfig()
+
+    if project is None:
+        project_config = "{}{}".format(CONFIG_PATH, bdconfigs['project config name'])
+
+    else:
+        project_config = "{}{}{}".format(project['project_dir'], os.sep, bdconfigs['project config name'])
+
+    with open(project_config) as json_data:
+        project_vars = QueryDict(json.load(json_data))
+
+    return project_vars
 
 # END FUNCTIONS -----------------------------------------------
-
-# MAIN PROGRAM --------------------------------------------
-def main():
-
-    with open('/Volumes/ProjectsRaid/x_Pipeline/x_AppPlugins/bd_pipeline/global_config.json') as json_data:
-        babvars = json.load(json_data)
-
-    print(json.dumps(babvars, sort_keys=True, indent=4))
-
-    with open("/Volumes/ProjectsRaid/x_Pipeline/x_AppPlugins/bd_pipeline/global_config_test.json", "w") as outfile:
-        json.dump(babvars, outfile, sort_keys=True, indent=4)
-
-    return babvars
-
-
-# END MAIN PROGRAM -----------------------------------------------
-
-if __name__ == '__main__':
-
-    try:
-        main()  # call the script with any arguments here.
-
-    except:
-        traceback.format_exc()
---
+++ vars.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# Alexander Kucera
+# babylondreams.de
+
+CONFIG_PATH = '/Volumes/ProjectsRaid/x_Pipeline/x_AppPlugins/bd_pipeline/global_config.json'
