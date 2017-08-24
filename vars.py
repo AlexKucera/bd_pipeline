2e1e06439a7087ae13cecb25da717f07e8b9e8e5
Alexander Kucera <a.kucera@babylondreams.de>
2017-08-24 14:14:08 +0200

    smarter auto config
    Signed-off-by: Alexander Kucera <a.kucera@babylondreams.de>

--- vars.py
+++ vars.py
@@ -3,4 +3,27 @@
 # Alexander Kucera
 # babylondreams.de
 
-CONFIG_PATH = '/Volumes/ProjectsRaid/x_Pipeline/x_AppPlugins/bd_pipeline/global_config.json'
+"""
+Global Variables
+
+Will try to find its own path and use the hardcoded path if it cannot find the global config file on its own.
+"""
+
+import bd_globals
+import os
+
+# Config
+
+CONFIG_PATH = '/Volumes/ProjectsRaid/x_Pipeline/x_AppPlugins/bd_pipeline/'
+CONFIG_NAME = 'global_config.json'
+
+# Automated Stuff.  No Touchy!
+
+globals_config_path = os.path.dirname(bd_globals.__file__) + os.sep + CONFIG_NAME
+
+if os.path.exists(globals_config_path):
+    CONFIG_PATH = globals_config_path
+else:
+    CONFIG_PATH += CONFIG_NAME
+
+
